ad4163620390c4b48cb01909abea73ba047fd8c6
asc793 <ahmetcuhadaroglu@hotmail.com>
2017-04-25 21:36:34 +0300

    -

---
+++ TeknolojiKovaniWebApi/Controllers/EnvironmentController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TeknolojiKovaniWebApi.Controllers
+{
+    public class EnvironmentController : ApiController
+    {
+        [Route("Environment/{environmentId}")]
+        [HttpGet]
+        public IHttpActionResult GetEnvironment(int environmentId)
+        {
+            return Ok();
+        }
+
+        [Route("Environment/{environmentId}/Devices")]
+        [HttpGet]
+        public IHttpActionResult GetEnvironmentDevices(int environmentId)
+        {
+            return Ok();
+        }
+    }
+}
