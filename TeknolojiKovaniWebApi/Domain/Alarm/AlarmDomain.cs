1f62d2626b4908467f8afd0f0a49052a01116edc
asc793 <ahmetcuhadaroglu@hotmail.com>
2017-04-28 22:17:40 +0300

    -

---
+++ TeknolojiKovaniWebApi/Domain/Alarm/AlarmDomain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeknolojiKovaniWebApi.Domain.Alarm
+{
+    public class AlarmDomain
+    {
+        internal void FireAlarm(DTOs.AlarmFireDto alarm)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
