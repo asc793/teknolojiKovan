d675a39150524757e71180c103ad50d9f75ffc1a
erkancelik <devilerkan@gmail.com>
2017-04-29 04:36:07 +0300

    Alarm ekleme silme ve listeleme eklendi

--- TeknolojiKovaniWebApi/Domain/Alarm/AlarmDomain.cs
+++ TeknolojiKovaniWebApi/Domain/Alarm/AlarmDomain.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using TeknolojiKovaniWebApi.Domain.Alarm.DTOs;
+using TeknolojiKovaniWebApi.Domain.Device.DTOs;
 using TeknolojiKovaniWebApi.Domain.Values;
 using TeknolojiKovaniWebApi.Domain.Values.DTOs.External;
 
@@ -44,6 +45,62 @@ namespace TeknolojiKovaniWebApi.Domain.Alarm
 
         private void AlarmSendEmail(AlarmNotification notification)
         {
+
+        }
+
+        public List<AlarmList> GetAlarmList(int UserId)
+        {
+
+            tKovanContext ctx = new tKovanContext();
+            //List<DTOs.External.Environment> lstEnvironment = ctx.Environment.Where(x => x.UserId == UserId).ToList().Select(x => new DTOs.External.Environment { Id = x.Id, Name = x.Name }).ToList();
+            //return lstEnvironment;
+            List<AlarmList> AlarmList = new List<AlarmList>();
+            List<DeviceList> device = ctx.Device.Where(x => x.UserId == UserId).ToList().Select(x => new DeviceList { CurrentToken = x.CurrentToken, DataSendInterval = x.DataSendInterval, EnvironmentName = x.Environment == null ? "" : x.Environment.Name, Id = x.Id, Name = x.Name, MacNo = x.MacNo, ProfileName = x.Profile == null ? "" : x.Profile.Name, UserName = x.User == null ? "" : x.User.UserName }).ToList();
+            foreach (DeviceList item in device)
+            {
+                List<TeknolojiKovaniWebApi.Models.EntityClass.Alarm> DeviceAlarm = ctx.Alarm.Where(x => x.DeviceId == item.Id).ToList();
+                foreach (TeknolojiKovaniWebApi.Models.EntityClass.Alarm it in DeviceAlarm)
+                {
+                    AlarmList dAlarm = new AlarmList();
+                    dAlarm = Utilities.Map<TeknolojiKovaniWebApi.Models.EntityClass.Alarm,AlarmList>(it, dAlarm);
+                    AlarmList.Add(dAlarm);
+                }
+            }
+
+            return AlarmList;
+        }
+
+        public bool SaveAlarm(AlarmList AlarmList)
+        {
+            try
+            {
+                tKovanContext ctx = new tKovanContext();
+                TeknolojiKovaniWebApi.Models.EntityClass.Alarm Alarm = new Models.EntityClass.Alarm();
+                Alarm = Utilities.Map<AlarmList, TeknolojiKovaniWebApi.Models.EntityClass.Alarm>(AlarmList, Alarm);
+                ctx.Alarm.Add(Alarm);
+                ctx.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public bool DeleteAlarm(int Id)
+        {
+            try
+            {
+                tKovanContext ctx = new tKovanContext();
+                Models.EntityClass.Alarm Alarm = ctx.Alarm.Single(x => x.Id == Id);
+                ctx.Alarm.Remove(Alarm);
+                ctx.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            
 
         }
     }
---
+++ TeknolojiKovaniWebApi/Domain/Alarm/DTOs/AlarmList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeknolojiKovaniWebApi.Models.EntityClass;
+
+namespace TeknolojiKovaniWebApi.Domain.Alarm.DTOs
+{
+    public class AlarmList
+    {
+        public int Id { get; set; }
+        public Guid DeviceId { get; set; } //
+        public int PropertyId { get; set; }//
+        public int Min { get; set; }//
+        public int Max { get; set; }//
+        public int PinNo { get; set; }//
+        public string AlarmParameter { get; set; }//
+        public AlarmType AlarmType { get; set; }//
+    }
+}
